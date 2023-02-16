fa9e6deee3df268f337fe79253331cfd6ef387b7
Joshua Blanchard <joshua.blanchard@ketch.com>
2023-02-15 18:03:35 -0800

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/sun-bear.jpeg") {
+    myImage.setAttribute("src", "images/my-lax-photo.jpg");
+  } else {
+    myImage.setAttribute("src", "images/sun-bear.jpeg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
