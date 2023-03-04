bdca03fe8761fb85126ca232f45ca94155a9ab66
Joshua Blanchard <joshua.blanchard@ketch.com>
2023-03-03 20:10:02 -0800

    added correct label to initial react component

--- scripts/basic_react_component.js
+++ scripts/basic_react_component.js
@@ -21,6 +21,6 @@ class LikeButton extends React.Component {
     }
 }
 
-const domContainer = document.querySelector('#like_button_container');
+const domContainer = document.querySelector('#basic_react_component');
 const root = ReactDOM.createRoot(domContainer);
 root.render(e(LikeButton));
